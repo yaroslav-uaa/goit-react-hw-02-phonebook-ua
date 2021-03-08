b71dff8c98df61f66128afea8bf073412ac98d5c
Yaroslav <ypakhachuk@gmail.com>
2021-03-08 21:00:44 +0200

    fix: update contacts

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import './App.css';
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-1', name: 'Busya Simpotusya', number: '459-12-56' },
+      { id: 'id-2', name: 'Jessica Parktronic', number: '443-89-12' },
+      { id: 'id-3', name: 'Silvester Vstolovoi', number: '645-17-79' },
+      { id: 'id-4', name: 'Tom Cruizcontrol', number: '227-91-26' },
     ],
     filter: '',
   };
